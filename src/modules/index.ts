2ae9c197c89acc1a7878de3f88dee0cb6171e081
LeeSeokHo <rrgks@naver.com>
2022-10-01 02:03:19 +0900

    Feat: example module 생성

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,5 @@
 import { HealthModule } from '@src/modules/health/health.module';
 import { CoreModule } from '@src/modules/core/core.module';
+import { ExampleModule } from '@src/modules/example/example.module';
 
-export const modules = [HealthModule, CoreModule];
+export const modules = [HealthModule, CoreModule, ExampleModule];
