599f92c50a63b140cf0074bbce67124b9c6a529a
LeeSeokHo <rrgks@naver.com>
2022-10-01 00:59:53 +0900

    Feat: helmet 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
+import helmet from 'helmet';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
+  app.use(helmet());
+
+  await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
