50c7395a6fce8323200f3a23db273ca785db03b7
LeeSeokHo <rrgks@naver.com>
2022-10-08 16:23:49 +0900

    Feat: prisma query logger 추가

--- src/modules/core/database/prisma/prisma.service.ts
+++ src/modules/core/database/prisma/prisma.service.ts
@@ -1,9 +1,51 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'error'>
+  implements OnModuleInit
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+
+    this.$on<any>('query', (event: Prisma.QueryEvent) => {
+      this.logger.debug('Query: ' + event.query);
+      this.logger.debug('Params: ' + event.params);
+      this.logger.debug('Duration: ' + event.duration + 'ms');
+    });
+  }
+
   async onModuleInit() {
+    this.$on('error', (event) => {
+      this.logger.verbose(event.target);
+    });
     await this.$connect();
   }
 
