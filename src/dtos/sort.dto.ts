fba717968d8bc81499a70e9c9e7392f7147c70f3
LeeSeokHo <rrgks@naver.com>
2022-10-10 00:33:55 +0900

    Fix: sortDto enum 적용

--- src/dtos/sort.dto.ts
+++ src/dtos/sort.dto.ts
@@ -1,18 +1,18 @@
 import { IsEnum, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { OrderBy } from '@src/constants/enum';
-import { PostSortField } from '@src/modules/post/constants/enum';
+import { PostField } from '@src/modules/post/constants/enum';
 import { IsEssential } from '@src/decorators/is-essential.decorator';
 
 export class SortDto {
   @ApiProperty({
     description: '정렬에 사용할 필드',
     required: false,
-    enum: PostSortField,
+    enum: PostField,
   })
   @IsOptional()
-  @IsEnum(PostSortField)
-  sortBy?: PostSortField;
+  @IsEnum(PostField)
+  sortBy?: PostField;
 
   @ApiProperty({
     description: '정렬순서 ASC : 오름차순, DESC : 내림차순',
