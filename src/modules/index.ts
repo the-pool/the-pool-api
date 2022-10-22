46bccf676b8a93c362b39c174b12fee881b9f999
subro <clark9810@naver.com>
2022-10-22 17:47:48 +0900

    :sparkles: Add HttpConfigModule
    ajax 요청을 위한 HttpConfigModule 추가

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,6 +3,7 @@ import { ExampleModule } from '@src/modules/example/example.module';
 import { CoreModule } from '@src/modules/core/core.module';
 import { UserModule } from '@src/modules/user/user.module';
 import { PostModule } from '@src/modules/post/post.module';
+import { HttpConfigModule } from './core/http/http-config.module';
 
 export const modules = [
   HealthModule,
@@ -10,4 +11,5 @@ export const modules = [
   CoreModule,
   UserModule,
   PostModule,
+  HttpConfigModule,
 ];
