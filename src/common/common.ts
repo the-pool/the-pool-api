f368f2c64ab61ca828e5874b0d9774e14d9a5973
subro <clark9810@naver.com>
2022-10-31 13:56:09 +0900

    :memo: #4 - common function 주석 추가
    common에 추가된 함수 주석 추가

--- src/common/common.ts
+++ src/common/common.ts
@@ -24,12 +24,18 @@ export const getValueByEnum = <T extends string | number>(
   }) as T[];
 };
 
+/**
+ * dto test 코드를 작성할 때 test하고 싶은 독립된 property를 만들어 낼 때 사용되는 함수
+ */
 export const customValidate = async <T>(property: {
   [key in keyof T]: any;
 }): Promise<ValidationError[]> => {
   return await validate(property, { skipUndefinedProperties: true });
 };
 
+/**
+ * swagger에 에러에 대한 문서를 만들어주기 위해 사용되는 함수
+ */
 export const ErrorResponseType = (
   description: string,
   status: number,
