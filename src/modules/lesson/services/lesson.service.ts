8d79745e51c06d86b9bc13d4f833fc6cebfa6d35
subro <clark9810@naver.com>
2022-11-09 19:43:15 +0900

    :memo: #29 - prisma schema Lesson
    Lesson 모델에 memberId 추가

--- src/modules/lesson/services/lesson.service.ts
+++ src/modules/lesson/services/lesson.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@src/modules/core/database/prisma/prisma.service';
+import { CreateLessonDto } from '../dtos/create-lesson.dto';
 
 @Injectable()
 export class LessonService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createLesson(createLessonDto, memberId) {}
+  async createLesson(createLessonDto: CreateLessonDto, memberId: number) {
+    // return await this.prismaService.lesson.create({
+    //   data: {
+    //     description: createLessonDto.description,
+    //     title: createLessonDto.title,
+    //     levelId: createLessonDto.levelId,
+    //   },
+    // });
+  }
 }
