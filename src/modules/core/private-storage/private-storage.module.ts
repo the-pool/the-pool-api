22d6ce7905ae8cdf02ae58caa4d7d00598dfbd3d
subro <clark9810@naver.com>
2022-11-14 19:38:59 +0900

    :recycle: #42 - private-storage > aws-s3-storage 파일명 변경
    private-storage에서 aws-s3-storage로 파일명 변경

--- src/modules/core/private-storage/private-storage.module.ts
+++ src/modules/core/private-storage/private-storage.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrivateStorageController } from './controllers/private-storage.controller';
 import { PRIVATE_STORAGE_SERVICE } from './interfaces/private-storage-service.interface';
-import { AwsS3StorageService } from './services/private-storage.service';
+import { AwsS3StorageService } from './services/aws-s3-storage.service';
 
 @Module({
   controllers: [PrivateStorageController],
