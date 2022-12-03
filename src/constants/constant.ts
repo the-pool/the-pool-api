4191e6e153d1febc469a159842864053a66a6f8c
subro <clark9810@naver.com>
2022-12-03 17:49:25 +0900

    :recycle: #63 - constant 변수명 변경
    global constant 변수명 카멜 > 대문자 스네이크 케이스로 변경

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,17 +1,33 @@
-import { LessonLevelId } from './enum';
-
-export const titleLength = {
+import { LessonLevelId, MajorId } from './enum';
+// 멤버 닉네임 길이 제한
+export const MEMBER_NICKNAME_LENGTH = {
+  // 1
   MIN: 1,
+  // 50
   MAX: 50,
-};
+} as const;
 
-export const nicknameLength = {
+// 레슨 제목 길이 제한
+export const LESSON_TITLE_LENGTH = {
+  // 1
   MIN: 1,
+  // 30
   MAX: 30,
-};
+} as const;
+
+// major text
+export const MAJOR_TEXT = {
+  // 개발
+  [MajorId.Development]: '개발',
+  // 디자인
+  [MajorId.Development]: '디자인',
+} as const;
 
 export const LESSON_LEVEL = {
+  // 난이도 상
   [LessonLevelId.Top]: '상',
+  // 난이도 중
   [LessonLevelId.Middle]: '중',
+  // 난이도 하
   [LessonLevelId.Bottom]: '하',
-};
+} as const;
--- src/modules/member/dtos/last-step-login.dto.ts
+++ src/modules/member/dtos/last-step-login.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { getEntriesByEnum } from '@src/common/common';
-import { nicknameLength } from '@src/constants/constant';
+import { MEMBER_NICKNAME_LENGTH } from '@src/constants/constant';
 import { MajorId, MajorSkillId } from '@src/constants/enum';
 import { IsRecord } from '@src/decorators/is-record.decorator';
 import {
@@ -19,7 +19,7 @@ export class LastStepLoginDto {
     example: 'thePool',
   })
   @IsRecord({ model: 'member' }, false)
-  @Length(nicknameLength.MIN, nicknameLength.MAX)
+  @Length(MEMBER_NICKNAME_LENGTH.MIN, MEMBER_NICKNAME_LENGTH.MAX)
   @IsString()
   nickname: string;
 
