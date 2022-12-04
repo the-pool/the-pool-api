8198cea793b0bccda7c77a4f75e8101d098e22cb
LeeSeokHo <rrgks@naver.com>
2022-12-04 20:15:57 +0900

    :memo: #51 - major relationFiled swagger 설명 추기

--- src/modules/major/dto/major-relation-field-request-query.dto.ts
+++ src/modules/major/dto/major-relation-field-request-query.dto.ts
@@ -5,6 +5,7 @@ import { IsBoolean, IsOptional } from 'class-validator';
 
 export class MajorRelationFieldRequestQueryDto {
   @ApiPropertyOptional({
+    description: 'mainSKills 를 true 로 주면 response 의 majors 가 추가됩니다.',
     default: false,
     enum: ['true', 'false'],
   })
