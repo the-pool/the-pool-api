ef89fe7d79bce2a73e9b8fb85fc40f54e69095f6
RokwonK <wonrok97@naver.com>
2022-12-10 17:08:32 +0900

    🎨 #62 - 데코레이터 순서 수정

--- src/modules/question/controllers/question.controller.ts
+++ src/modules/question/controllers/question.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/
 import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CustomApiResponse } from '@src/decorators/custom-api-response.decorator';
 import { JwtAuthGuard } from '@src/guards/jwt-auth.guard';
+import { QuestionDto } from '../dtos/question.dto';
 import { QuestionService } from '../services/question.service';
 
 @ApiTags('QnA')
@@ -9,25 +10,25 @@ import { QuestionService } from '../services/question.service';
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) { }
 
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: '질문 생성' })
   @ApiBearerAuth()
-  @ApiCreatedResponse({ type: LessonEntity })
+  @ApiCreatedResponse({ type: QuestionDto })
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.CREATED)
   @CustomApiResponse(HttpStatus.UNAUTHORIZED, 'Unauthorized')
   @CustomApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, '서버 에러')
-  async createQuestion() {
+  @Post()
+  async createQuestion(
 
-  }
-
-  @Get('questionList')
-  async getQuestionList() {
-
+  ) {
   }
 
   @Get('categoryList')
   async getQuestionCategoryList() {
 
   }
+  // @Get('questionList')
+  // async getQuestionList() {
+
+  // }
 }
