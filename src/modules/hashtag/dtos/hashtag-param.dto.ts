8c9eced68d17a8a116cd3bc5ed918939935f5788
subro <clark9810@naver.com>
2023-01-04 11:27:54 +0900

    :recycle: #70 - LessonHashtagParamDto, IdRequestParamDto IsInt 추가
    params id 에 float 자료형이 들어와도 대응될 수 있도록 수정

--- src/modules/hashtag/dtos/hashtag-param.dto.ts
+++ src/modules/hashtag/dtos/hashtag-param.dto.ts
@@ -3,7 +3,7 @@ import { ModelName } from '@src/constants/enum';
 import { IsRecord } from '@src/decorators/is-record.decorator';
 import { IdRequestParamDto } from '@src/dtos/id-request-param.dto';
 import { Type } from 'class-transformer';
-import { Min } from 'class-validator';
+import { IsInt, IsNumber, Min } from 'class-validator';
 
 export class LessonHashtagParamDto extends IdRequestParamDto {
   @ApiProperty({
@@ -13,6 +13,7 @@ export class LessonHashtagParamDto extends IdRequestParamDto {
   })
   @IsRecord({ model: ModelName.LessonHashtag, field: 'id' }, true)
   @Min(1)
+  @IsInt()
   @Type(() => Number)
   hashtagId: number;
 }
