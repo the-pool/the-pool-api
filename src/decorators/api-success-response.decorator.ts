061acc1a12a67402ff3390a44274dbdd9441eb5e
subro <clark9810@naver.com>
2023-01-10 15:56:15 +0900

    :bug: #72 - develop 과 충돌 해결.
    충돌해결

--- src/decorators/api-success-response.decorator.ts
+++ src/decorators/api-success-response.decorator.ts
@@ -71,7 +71,6 @@ type Primitive = OptionalProperty & {
  * 성공에 대한 응답을 swagger상에 보여주고 싶은데 필드명을 커스텀 하고 싶을 때
  * 사용하는 데코레이터이며 원하는데로 필드와 타입을 지정해 줄 수 있다.
  */
-<<<<<<< HEAD
 
 export const ApiSuccessResponse = (
   status: Exclude<HttpStatus, ErrorHttpStatusCode>,
@@ -155,27 +154,5 @@ export const ApiSuccessResponse = (
     HttpCode(status),
     ApiExtraModels(...extraModels),
     ApiResponse(apiResponseOption),
-=======
-export const ApiSuccessResponse = (
-  status: Exclude<HttpStatus, ErrorHttpStatusCode>,
-  props: { [key: string]: Type },
-) => {
-  const extraModels: Type[] = [];
-  const properties: Record<string, ReferenceObject> = {};
-
-  Object.entries(props).forEach(([key, type]) => {
-    extraModels.push(type);
-    properties[key] = { $ref: getSchemaPath(type) };
-  });
-
-  return applyDecorators(
-    ApiExtraModels(...extraModels),
-    ApiResponse({
-      status,
-      schema: {
-        properties,
-      },
-    }),
->>>>>>> a52b8f7be2906834f331ada8531df8d2212eda46
   );
 };
