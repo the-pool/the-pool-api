68a3d99abfa87b4dea61e7f814687e36f8b6e493
subro <clark9810@naver.com>
2023-01-12 19:19:11 +0900

    :recycle: #72 - update-evaluaion, lesson-evaluation.entity 프로퍼티 수정
    프로퍼티 수정

--- src/modules/lesson/dtos/evaluation/update-evaluation.dto.ts
+++ src/modules/lesson/dtos/evaluation/update-evaluation.dto.ts
@@ -1,13 +1,16 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { getEntriesByEnum } from '@src/common/common';
 import { LessonLevelId } from '@src/constants/enum';
-import { IsOptional } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 import { CreateEvaluationDto } from './create-evaluation.dto';
 
 export class UpdateEvaluationDto extends CreateEvaluationDto {
-  @ApiProperty({
-    example: 1,
+  @ApiPropertyOptional({
+    example: LessonLevelId.Top,
     description: '과제를 수행한 member가 느끼는 과제의 난이도',
+    enum: getEntriesByEnum(LessonLevelId),
   })
+  @IsEnum(LessonLevelId)
   @IsOptional()
   levelId: LessonLevelId;
 }
