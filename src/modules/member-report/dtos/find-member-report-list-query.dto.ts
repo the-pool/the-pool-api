52998bde4a99fff8830fcc9a89f1dee18604c259
LeeSeokHo <rrgks@naver.com>
2023-01-14 18:42:44 +0900

    :sparkles: #55 - member report list 조회 시 memberId 제거

--- src/modules/member-report/dtos/find-member-report-list-query.dto.ts
+++ src/modules/member-report/dtos/find-member-report-list-query.dto.ts
@@ -1,20 +1,8 @@
-import { ApiPropertyOptional, IntersectionType } from '@nestjs/swagger';
+import { IntersectionType } from '@nestjs/swagger';
 import { PageDto } from '@src/dtos/page.dto';
 import { SortDto } from '@src/dtos/sort.dto';
-import { Type } from 'class-transformer';
-import { IsInt, IsOptional, Min } from 'class-validator';
 
 export class FindMemberReportListQueryDto extends IntersectionType(
   PageDto,
   SortDto,
-) {
-  @ApiPropertyOptional({
-    description: 'member 고유 Id',
-    minimum: 1,
-  })
-  @Min(1)
-  @IsInt()
-  @IsOptional()
-  @Type(() => Number)
-  memberId?: number;
-}
+) {}
