f5a9eab870429bc0c773205022c2acfcbfd19dd1
subro <clark9810@naver.com>
2023-01-14 18:17:23 +0900

    :recycle: #72 - updateEvaluationDto class-validator 수정
    class-validator 수정

--- src/modules/lesson/dtos/evaluation/update-evaluation.dto.ts
+++ src/modules/lesson/dtos/evaluation/update-evaluation.dto.ts
@@ -2,15 +2,14 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { getEntriesByEnum, getValueByEnum } from '@src/common/common';
 import { LessonLevelId } from '@src/constants/enum';
 import { IsEnum, IsIn, IsOptional } from 'class-validator';
-import { CreateEvaluationDto } from './create-evaluation.dto';
 
-export class UpdateEvaluationDto extends CreateEvaluationDto {
+export class UpdateEvaluationDto {
   @ApiPropertyOptional({
     example: LessonLevelId.Top,
     description: '과제를 수행한 member가 느끼는 과제의 난이도',
     enum: getEntriesByEnum(LessonLevelId),
-    nullable: true,
   })
-  @IsIn([...getValueByEnum(LessonLevelId, 'number'), null])
-  levelId: LessonLevelId;
+  @IsEnum(LessonLevelId)
+  @IsOptional()
+  levelId: LessonLevelId | null = null;
 }
