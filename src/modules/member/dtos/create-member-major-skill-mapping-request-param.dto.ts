4ebe1a60a02ec08ad5cba02082b5e475d9a2d718
LeeSeokHo <rrgks@naver.com>
2023-01-28 17:29:50 +0900

    :sparkles: #79 - majorSkillsId csv 형태로 받게끔 dto 수정

--- src/modules/member/dtos/create-member-major-skill-mapping-request-param.dto.ts
+++ src/modules/member/dtos/create-member-major-skill-mapping-request-param.dto.ts
@@ -1,14 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Major, MajorSkill } from '@prisma/client';
-import { ModelName } from '@src/constants/enum';
+import { Major } from '@prisma/client';
+import {
+  transformCsvToArray,
+  transformEachStrungToNumber,
+  transformEachTrim,
+} from '@src/common/common';
 import { IsRecord } from '@src/decorators/is-record.decorator';
 import { IdRequestParamDto } from '@src/dtos/id-request-param.dto';
-import { Type } from 'class-transformer';
-import { IsInt, Min } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { ArrayUnique, IsInt, Min } from 'class-validator';
 
 export class CreateMemberMajorSkillMappingRequestParamDto extends IdRequestParamDto {
   @ApiProperty({
     description: 'major 고유 ID',
+    minimum: 1,
   })
   @IsRecord<Major>({ model: 'major', field: 'id' }, true)
   @Min(1)
@@ -17,11 +22,17 @@ export class CreateMemberMajorSkillMappingRequestParamDto extends IdRequestParam
   majorId: number;
 
   @ApiProperty({
-    description: 'major skill 고유 ID',
+    description:
+      'major skill 고유 ID\nCSV 형태로 보내줘야합니다.\n, 기준으로 앞뒤 공백 제거한 뒤 유효성 검사 진행합니다.',
+    example: '1, 2, 3',
+    minimum: 1,
+    uniqueItems: true,
   })
-  @IsRecord<MajorSkill>({ model: ModelName.MajorSkill, field: 'id' }, true)
-  @Min(1)
-  @IsInt()
-  @Type(() => Number)
-  majorSkillId: number;
+  @Min(1, { each: true })
+  @IsInt({ each: true })
+  @ArrayUnique()
+  @Transform(transformCsvToArray)
+  @Transform(transformEachTrim)
+  @Transform(transformEachStrungToNumber)
+  majorSkillIds: number;
 }
