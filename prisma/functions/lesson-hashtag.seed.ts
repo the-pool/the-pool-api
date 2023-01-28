e9bb9632984f05a12628a6e7c138fa14be2f425e
subro <clark9810@naver.com>
2023-01-28 22:12:57 +0900

    :sparkles: #74 - LessonHashtagMapping, createDataForSeed
    과제 해시태그 매핑 테이블 스키마 생성 및 seed데이터 만들어주는 함수 생성

---
+++ prisma/functions/lesson-hashtag.seed.ts
@@ -0,0 +1,50 @@
+import { createDataForSeed } from '../seed';
+
+export const lessonHashtagSeed = async (prisma) => {
+  enum DevelopHashtag {
+    Backend = '백엔드개발',
+    WebFrontend = '웹 프론트엔드',
+    Ios = 'IOS',
+    Android = '안드로이드',
+    Etc = '기타 개발',
+    Javascript = 'Javascript',
+    React = 'React',
+    Vue = 'Vue',
+    Angular = 'Angular',
+    Node = 'Node.js',
+    Java = 'Java',
+    Python = 'Python',
+    PHP = 'PHP',
+    InfraStructure = 'InfraStructure',
+    Database = 'Database',
+    Git = 'Git',
+    BigData = '빅데이터',
+    AI = 'AI',
+    MachineLearning = 'machineLearning',
+    C = 'C',
+  }
+
+  enum AlgorithmHashtag {
+    Greedy = '그리디',
+    Implementation = '구현',
+    DFS = 'DFS',
+    BFS = 'BFS',
+    Sorting = '정렬',
+    BinarySearch = '이진 탐색',
+    DynamicProgramming = '다이나믹 프로그래밍',
+    ShortestPath = '최단 경로',
+    GraphTheory = '그래프 이론',
+  }
+
+  const developHashtags = createDataForSeed(DevelopHashtag, 'string', 'tag');
+  const algorithmHashtags = createDataForSeed(
+    AlgorithmHashtag,
+    'string',
+    'tag',
+  );
+
+  await prisma.lessonHashtag.createMany({
+    data: [...developHashtags, ...algorithmHashtags],
+    skipDuplicates: true,
+  });
+};
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import { getValueByEnum } from '../src/common/common';
 import { lessonCategorySeed } from './functions/lesson-category.seed';
+import { lessonHashtagSeed } from './functions/lesson-hashtag.seed';
 import { lessonLevelSeed } from './functions/lesson-level.seed';
 import { majorSkillSeed } from './functions/major-skill.seed';
 import { majorSeed } from './functions/major.seed';
@@ -9,11 +11,29 @@ import { memberInterestSeed } from './functions/member-interest.seed';
 const prisma = new PrismaClient();
 
 /**
+ * seed 파일에서 사용되는 createMany의 data자리에 들어갈 값을 만드는 메서드
+ * { id : 1, fieldName : "값" } 형식을 사용되며
+ * getValueByEnum을 사용하기 때문에 fieldName에 value로 들어갈 자료형을 함께 넘겨주어야 함.
+ */
+export const createDataForSeed = (
+  Enum: Record<string, string | number>,
+  type: 'number' | 'string',
+  fieldName: string,
+) => {
+  return getValueByEnum(Enum, type).reduce((acc, cur, idx) => {
+    acc.push({ id: ++idx, [fieldName]: cur });
+
+    return acc;
+  }, [] as { id: number; [fieldName: string]: string | number }[]);
+};
+
+/**
  * the pool seed
  */
 async function thePoolSeed() {
   await lessonCategorySeed(prisma);
   await lessonLevelSeed(prisma);
+  await lessonHashtagSeed(prisma);
   await majorSeed(prisma);
   await majorSeed(prisma);
   await memberInterestSeed(prisma);
