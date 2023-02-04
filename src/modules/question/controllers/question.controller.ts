1bd68b1f79d957570c2696c5d75d3891a3f2d7e6
RokwonK <wonrok97@naver.com>
2023-02-04 18:02:01 +0900

    ✨ #89 - get question category list 핸들러 추가

--- src/modules/question/controllers/question.controller.ts
+++ src/modules/question/controllers/question.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, InternalServerErrorException, UseGuards } from '@nestjs/common';
+import { ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
+import { QuestionCategory } from '@prisma/client';
+import { JwtAuthGuard } from '@src/guards/jwt-auth.guard';
+import { NotFoundResponseType } from '@src/types/not-found-response.type';
+import { QuestionCategoryEntity } from '../entities/question-category.entity';
+import { QuestionService } from '../services/question.service';
 
-@Controller('question')
-export class QuestionController {}
+@ApiTags('커뮤니티 (QnA)')
+@ApiNotFoundResponse({ type: NotFoundResponseType })
+@ApiInternalServerErrorResponse({ type: InternalServerErrorException })
+@Controller('api/question')
+export class QuestionController {
+  constructor(
+    private readonly questionService: QuestionService
+  ) { }
+
+  @Get('/categories')
+  findQuestionCategoryList(): Promise<QuestionCategoryEntity[]> {
+    return this.questionService.findQuestionCategoryList();
+  }
+}
