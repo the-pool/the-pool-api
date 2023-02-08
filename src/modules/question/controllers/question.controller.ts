ff3fcaf2649513d03f2e9a0228d6c1e9a558042a
RokwonK <wonrok97@naver.com>
2023-02-08 16:23:49 +0900

    ✨ #89 - Question Category List Swagger 데코레이터 추가

--- src/modules/question/controllers/question.controller.ts
+++ src/modules/question/controllers/question.controller.ts
@@ -1,20 +1,19 @@
 import { Controller, Get, InternalServerErrorException, UseGuards } from '@nestjs/common';
 import { ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
-import { QuestionCategory } from '@prisma/client';
 import { JwtAuthGuard } from '@src/guards/jwt-auth.guard';
 import { NotFoundResponseType } from '@src/types/not-found-response.type';
 import { QuestionCategoryEntity } from '../entities/question-category.entity';
 import { QuestionService } from '../services/question.service';
+import { ApiFindQuestionCategoryList } from './question.swagger';
 
 @ApiTags('커뮤니티 (QnA)')
-@ApiNotFoundResponse({ type: NotFoundResponseType })
-@ApiInternalServerErrorResponse({ type: InternalServerErrorException })
 @Controller('api/questions')
 export class QuestionController {
   constructor(
     private readonly questionService: QuestionService
   ) { }
 
+  @ApiFindQuestionCategoryList('QnA 카테고리 리스트 조회')
   @Get('/categories')
   findQuestionCategoryList(): Promise<QuestionCategoryEntity[]> {
     return this.questionService.findQuestionCategoryList();
