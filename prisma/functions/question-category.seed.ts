909146eab9d0d31251424037831c73df0e688927
RokwonK <wonrok97@naver.com>
2023-02-11 18:42:14 +0900

    🎨 #94 - question category seed 수정

--- prisma/functions/question-category.seed.ts
+++ prisma/functions/question-category.seed.ts
@@ -1,10 +1,10 @@
 export const questionCategorySeed = async (prisma) => {
   enum CategoryName {
+    Etc = '기타',
     Develoment = '개발',
     Design = '디자인',
     Career = '커리어',
-    WorkLife = '직장생활',
-    Etc = '기타'
+    WorkLife = '직장생활'
   }
 
   await prisma.questionCategory.createMany({
