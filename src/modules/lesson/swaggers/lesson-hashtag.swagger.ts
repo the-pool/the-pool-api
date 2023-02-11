0be02633e5f9c5a169481b4097e1a9a1b12cddae
subro <clark9810@naver.com>
2023-02-11 21:13:04 +0900

    :memo: #88 - lesson-hashtag swagger 수정
    ApiExtraModels 1회만 될 수 있도록 수정

--- src/modules/lesson/swaggers/lesson-hashtag.swagger.ts
+++ src/modules/lesson/swaggers/lesson-hashtag.swagger.ts
@@ -14,7 +14,7 @@ import { LessonHashtagEntity } from '../entities/lesson-hashtag.entity';
 export const ApiCreateManyHashtag = (summary: string) => {
   return applyDecorators(
     ApiOperation({ summary }),
-    ApiExtraModels(LessonHashtagMappingEntity),
+    ApiExtraModels(LessonHashtagMappingEntity, LessonHashtagEntity),
     ApiCreatedResponse({
       schema: {
         properties: {
@@ -61,7 +61,10 @@ export const APiDeleteManyHashtag = (summary: string) => {
     ApiOperation({ summary }),
     ApiOkResponse({ schema: { example: { count: 1 } } }),
     ApiFailureResponse(HttpStatus.FORBIDDEN, HTTP_ERROR_MESSAGE.FORBIDDEN),
-    ApiFailureResponse(HttpStatus.NOT_FOUND, HTTP_ERROR_MESSAGE.NOT_FOUND),
+    ApiFailureResponse(HttpStatus.NOT_FOUND, [
+      HTTP_ERROR_MESSAGE.NOT_FOUND,
+      `{modelName}에 존재하지 않는 관계 입니다.`,
+    ]),
   );
 };
 
