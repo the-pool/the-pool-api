5e44b5ffc9fd6e2f0aa3c44464024031150802fc
LeeSeokHo <rrgks@naver.com>
2023-02-11 22:20:09 +0900

    :bug: #96 - common helper injectable 데코레이터 누락된 부분 추가

--- src/helpers/common.helper.ts
+++ src/helpers/common.helper.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
 export class CommonHelper {
   setSeparator(separator: string, ...strings: string[]): string {
     return strings.join(separator);
