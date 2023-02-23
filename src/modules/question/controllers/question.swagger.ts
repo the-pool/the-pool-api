f3c3f2e7f229dd46376696a25222ee777518d5cc
RokwonK <wonrok97@naver.com>
2023-02-23 19:42:12 +0900

    ✨ #94 - Question 생성 swagger 문서 추가

--- src/modules/question/controllers/question.swagger.ts
+++ src/modules/question/controllers/question.swagger.ts
@@ -1,11 +1,22 @@
 import { applyDecorators, HttpStatus } from "@nestjs/common"
-import { ApiOkResponse, ApiOperation } from "@nestjs/swagger"
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiBearerAuth } from "@nestjs/swagger"
+import { ApiFailureResponse } from "@src/decorators/api-failure-response.decorator"
 import { ApiSuccessResponse } from "@src/decorators/api-success-response.decorator"
 import { QuestionCategoryEntity } from "../entities/question-category.entity"
+import { QuestionEntity } from "../entities/question.entity"
 
 export const ApiFindQuestionCategoryList = (summary: string) => {
   return applyDecorators(
     ApiOperation({ summary }),
     ApiOkResponse({ type: QuestionCategoryEntity, isArray: true })
+  )
+}
+
+export const ApiCreateQuestion = (summary: string) => {
+  return applyDecorators(
+    ApiOperation({ summary }),
+    ApiBearerAuth(),
+    ApiCreatedResponse({ type: QuestionEntity }),
+    ApiFailureResponse(HttpStatus.UNAUTHORIZED, 'Unauthorized'),
   )
 }
