3260abce2ea79d8286136ce60128b835780bb078
RokwonK <wonrok97@naver.com>
2023-02-23 19:43:40 +0900

    ✨ #94 - Question 생성 controller 테스트 추가

--- src/modules/question/controllers/question.controller.spec.ts
+++ src/modules/question/controllers/question.controller.spec.ts
@@ -1,14 +1,18 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '@src/modules/core/database/prisma/prisma.service';
+import { MockClassType } from 'test/mock/mock.type';
 import { mockPrismaService } from '../../../../test/mock/mock-prisma-service';
 import { mockQuestionService } from '../../../../test/mock/mock-services';
+import { CreateQuestionRequestBodyDto } from '../dtos/create-question-request-body.dto';
 import { QuestionCategoryEntity } from '../entities/question-category.entity';
+import { QuestionEntity } from '../entities/question.entity';
 import { QuestionService } from '../services/question.service';
 import { QuestionController } from './question.controller';
 
 describe('QuestionController', () => {
   let questionController: QuestionController;
-  let questionService;
+  let questionService: MockClassType<QuestionService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,6 +26,7 @@ describe('QuestionController', () => {
     }).compile();
 
     questionController = module.get<QuestionController>(QuestionController);
+
     questionService = mockQuestionService;
   });
 
@@ -46,6 +51,33 @@ describe('QuestionController', () => {
 
       expect(questionService.findQuestionCategoryList).toBeCalledTimes(1);
       expect(result).toStrictEqual(categoryList);
+    })
+  })
+
+  describe('Create Question', () => {
+    let createQuestionRequestBodyDto: CreateQuestionRequestBodyDto;
+    let memberId: number
+    let createdQuestion: QuestionEntity
+
+    beforeEach(() => {
+      memberId = faker.datatype.number();
+      createQuestionRequestBodyDto = {
+        categoryId: 1,
+        title: 'test Title',
+        content: 'test Content'
+      };
+      createdQuestion = new QuestionEntity();
+      questionService.createQuestion.mockReturnValue(createdQuestion);
+    })
+
+    it('SUCCESS - 질문 생성', async () => {
+      const result = await questionService.createQuestion(
+        createQuestionRequestBodyDto,
+        memberId
+      );
+
+      expect(questionService.createQuestion).toBeCalledTimes(1);
+      expect(result).toStrictEqual(createdQuestion);
     })
   })
 });
