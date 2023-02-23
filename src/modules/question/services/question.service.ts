851abaa33cb755836c5c6d42b3ebedfbbf8f7bdb
RokwonK <wonrok97@naver.com>
2023-02-23 19:39:47 +0900

    ✨ #94 - Question 생성 API 구현

--- src/modules/question/services/question.service.ts
+++ src/modules/question/services/question.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { QuestionCategory } from '@prisma/client';
 import { PrismaService } from '@src/modules/core/database/prisma/prisma.service';
+import { CreateQuestionRequestBodyDto } from '../dtos/create-question-request-body.dto';
 import { QuestionCategoryEntity } from '../entities/question-category.entity';
+import { QuestionEntity } from '../entities/question.entity';
 
 @Injectable()
 export class QuestionService {
@@ -9,7 +11,25 @@ export class QuestionService {
     private readonly prismaService: PrismaService
   ) { }
 
+  /**
+   * QnA 카테고리 불러오기
+   */
   findQuestionCategoryList(): Promise<QuestionCategoryEntity[]> {
     return this.prismaService.questionCategory.findMany();
+  }
+
+  /**
+   * Question 생성
+   */
+  createQuestion(
+    question: CreateQuestionRequestBodyDto,
+    memberId: number
+  ): Promise<QuestionEntity> {
+    return this.prismaService.question.create({
+      data: {
+        ...question,
+        memberId
+      }
+    })
   }
 }
