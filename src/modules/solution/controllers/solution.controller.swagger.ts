10f93f603c84e645d9ea698f1fb5c7c07fe36f08
RokwonK <wonrok97@naver.com>
2023-02-28 21:10:48 +0900

    ✨ #114 - 생성완료 API Doc 추가

--- src/modules/solution/controllers/solution.controller.swagger.ts
+++ src/modules/solution/controllers/solution.controller.swagger.ts
@@ -1,7 +1,8 @@
 import { applyDecorators } from "@nestjs/common";
-import { ApiOperation } from "@nestjs/swagger";
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation } from "@nestjs/swagger";
+import { SolutionEntity } from "../entities/solution.entity";
 
 export const ApiCreateSolution = (summary: string) => applyDecorators(
   ApiOperation({ summary }),
-
+  ApiCreatedResponse({ type: SolutionEntity })
 );
