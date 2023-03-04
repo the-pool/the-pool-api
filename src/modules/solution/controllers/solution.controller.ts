ed5a6d25246e3e066f80dfbe878f8686b76a3443
RokwonK <wonrok97@naver.com>
2023-03-04 17:15:25 +0900

    ✨ #114 - 문제풀이 제출 시 member active 검사 및 count증가 인터셉터 추가

--- src/modules/solution/controllers/solution.controller.ts
+++ src/modules/solution/controllers/solution.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, InternalServerErrorException, Post, UseGuards } from '@nestjs/common';
-import { ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { IncreaseMemberSolutionCountInterceptor } from '@src/decorators/increase-member-solution-count.ineterceptor.decorator';
+import { AllowMemberStatusesSetMetadataGuard } from '@src/decorators/member-statuses-set-metadata.guard-decorator';
 import { UserLogin } from '@src/decorators/user-login.decorator';
 import { JwtAuthGuard } from '@src/guards/jwt-auth.guard';
-import { NotFoundResponseType } from '@src/types/not-found-response.type';
+import { MemberStatus } from '@src/modules/member/constants/member.enum';
 import { CreateSolutionRequestBodyDto } from '../dtos/create-solution-request-body.dto';
 import { SolutionEntity } from '../entities/solution.entity';
 import { SolutionService } from '../services/solution.service';
@@ -11,19 +13,17 @@ import { ApiCreateSolution } from './solution.controller.swagger';
 @ApiTags('문제 - 풀이')
 @Controller('api/solutions')
 export class SolutionController {
-  constructor(
-    private readonly solutionService: SolutionService
-  ) { }
+  constructor(private readonly solutionService: SolutionService) {}
 
   @ApiCreateSolution('문제 - 풀이 생성')
+  @IncreaseMemberSolutionCountInterceptor('increment')
+  @AllowMemberStatusesSetMetadataGuard([MemberStatus.Active])
   @UseGuards(JwtAuthGuard)
   @Post()
   createSolution(
     @Body() requestDto: CreateSolutionRequestBodyDto,
-    @UserLogin('id') memberId: number
+    @UserLogin('id') memberId: number,
   ): Promise<SolutionEntity> {
-    return this.solutionService.createSolution(
-      requestDto, memberId
-    );
+    return this.solutionService.createSolution(requestDto, memberId);
   }
 }
