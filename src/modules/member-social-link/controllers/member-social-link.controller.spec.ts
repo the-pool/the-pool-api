645865a3b3ae2ce822066d1fcf7000e268505653
LeeSeokHo <rrgks@naver.com>
2023-03-04 19:49:42 +0900

    :white_check_mark: controller test code

--- src/modules/member-social-link/controllers/member-social-link.controller.spec.ts
+++ src/modules/member-social-link/controllers/member-social-link.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MemberSocialLinkEntity } from '@src/modules/member-social-link/entities/member-social-link.entity';
+import { mockMemberSocialLinkService } from '../../../../test/mock/mock-services';
 import { MemberSocialLinkService } from '../services/member-social-link.service';
 import { MemberSocialLinkController } from './member-social-link.controller';
 
@@ -8,7 +10,12 @@ describe('MemberSocialLinkController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MemberSocialLinkController],
-      providers: [MemberSocialLinkService],
+      providers: [
+        {
+          provide: MemberSocialLinkService,
+          useValue: mockMemberSocialLinkService,
+        },
+      ],
     }).compile();
 
     controller = module.get<MemberSocialLinkController>(
@@ -18,5 +25,25 @@ describe('MemberSocialLinkController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    let memberSocialLinkEntities: MemberSocialLinkEntity[];
+
+    beforeEach(() => {
+      memberSocialLinkEntities = [new MemberSocialLinkEntity()];
+
+      mockMemberSocialLinkService.findAll.mockRestore();
+    });
+
+    it('성공', () => {
+      mockMemberSocialLinkService.findAll.mockResolvedValue(
+        memberSocialLinkEntities,
+      );
+
+      expect(controller.findAll()).resolves.toStrictEqual(
+        memberSocialLinkEntities,
+      );
+    });
   });
 });
--- test/mock/mock-services.ts
+++ test/mock/mock-services.ts
@@ -10,11 +10,11 @@ import { MajorService } from '@src/modules/major/services/major.service';
 import { MemberFriendshipService } from '@src/modules/member-friendship/services/member-friendship.service';
 import { MemberInterestService } from '@src/modules/member-interest/services/member-interest.service';
 import { MemberSkillService } from '@src/modules/member-skill/services/member-skill.service';
+import { MemberSocialLinkService } from '@src/modules/member-social-link/services/member-social-link.service';
 import { MemberStatisticsService } from '@src/modules/member-statistics/services/member-statistics.service';
 import { MemberValidationService } from '@src/modules/member/services/member-validation.service';
 import { MemberService } from '@src/modules/member/services/member.service';
 import { QuestionService } from '@src/modules/question/services/question.service';
-import { JestMockExtended } from 'jest-mock-extended';
 import { MockClassType } from './mock.type';
 
 export const mockConfigService = {
@@ -60,6 +60,11 @@ export const mockMemberInterestService: MockClassType<MemberInterestService> = {
 export const mockMemberSkillService: MockClassType<MemberSkillService> = {
   findAll: jest.fn(),
 };
+
+export const mockMemberSocialLinkService: MockClassType<MemberSocialLinkService> =
+  {
+    findAll: jest.fn(),
+  };
 
 export const mockMemberValidationService: MockClassType<MemberValidationService> =
   {
