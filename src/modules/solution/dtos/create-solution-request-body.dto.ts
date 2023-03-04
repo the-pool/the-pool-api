cd0cdf671401336f402e482f06d9fd744d38766c
RokwonK <wonrok97@naver.com>
2023-03-04 18:17:01 +0900

    ✨ #114 - relatedLink undefined 처리 추가

--- src/modules/solution/dtos/create-solution-request-body.dto.ts
+++ src/modules/solution/dtos/create-solution-request-body.dto.ts
@@ -1,13 +1,25 @@
-import { PartialType, PickType } from "@nestjs/swagger";
-import { Lesson, LessonSolution } from "@prisma/client";
-import { IsRecord } from "@src/decorators/is-record.decorator";
-import { IsDataURI, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, IsUrl, Min, MinLength } from "class-validator";
-import { SolutionEntity } from "../entities/solution.entity";
+import { isNil } from '@nestjs/common/utils/shared.utils';
+import { PartialType, PickType } from '@nestjs/swagger';
+import { Lesson, LessonSolution } from '@prisma/client';
+import { IsRecord } from '@src/decorators/is-record.decorator';
+import {
+  IsDataURI,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Min,
+  MinLength,
+  ValidateIf,
+} from 'class-validator';
+import { SolutionEntity } from '../entities/solution.entity';
 
 export class CreateSolutionRequestBodyDto extends PickType(SolutionEntity, [
   'lessonId',
   'description',
-  'relatedLink'
+  'relatedLink',
 ]) {
   @IsRecord<Lesson>({ model: 'lesson', field: 'id' }, false)
   @IsInt()
@@ -21,6 +33,6 @@ export class CreateSolutionRequestBodyDto extends PickType(SolutionEntity, [
 
   @IsUrl({ require_protocol: true, require_valid_protocol: true })
   @IsString()
-  @IsOptional()
-  relatedLink: string;
+  @ValidateIf((a) => !isNil(a.relatedLink))
+  relatedLink: string | null = null;
 }
