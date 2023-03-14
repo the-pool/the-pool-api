52464e765c509d86bf46f8b6f6393f278801493a
RokwonK <wonrok97@naver.com>
2023-03-14 13:29:23 +0900

    ✨ #124 - solution 동적 라우팅 추가

--- src/modules/solution/solution.module.ts
+++ src/modules/solution/solution.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { JwtStrategy } from '../core/auth/jwt/jwt.strategy';
 import { PrismaService } from '../core/database/prisma/prisma.service';
 import { NotificationModule } from '../core/notification/notification.module';
+import { SolutionHashtagController } from './controllers/solution-hashtag.controller';
 import { SolutionController } from './controllers/solution.controller';
+import { SolutionHashtagService } from './services/solution-hashtag.service';
 import { SolutionService } from './services/solution.service';
 
 @Module({
-  imports: [NotificationModule],
-  controllers: [SolutionController],
-  providers: [SolutionService, PrismaService],
+  imports: [
+    NotificationModule,
+    RouterModule.register([{ path: 'api/solutions', module: SolutionModule }]),
+  ],
+  providers: [SolutionService, SolutionHashtagService, PrismaService],
+  controllers: [SolutionController, SolutionHashtagController],
 })
 export class SolutionModule {}
