2358a0eec282357db326b2d7765143a2cb4458da
RokwonK <wonrok97@naver.com>
2023-03-14 13:34:08 +0900

    ✨ #124 - solution hashtag swagger 추가

---
+++ src/modules/solution/controllers/solution-hashtag.controller.swagger.ts
@@ -0,0 +1,17 @@
+import { applyDecorators, HttpStatus } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { ApiFailureResponse } from '@src/decorators/api-failure-response.decorator';
+import { SolutionHashtagEntity } from '../entities/solution-hashtag.entity';
+
+export const ApiCreateManySolutionHashtag = (summary: string) =>
+  applyDecorators(
+    ApiOperation({ summary }),
+    ApiCreatedResponse({ type: SolutionHashtagEntity, isArray: true }),
+    ApiFailureResponse(HttpStatus.BAD_REQUEST, [
+      '등록 가능한 해시태그의 최대갯수는 5개입니다.',
+    ]),
+  );
