b1a5bc05c2f0e312b9ea10717693cf2b3f21be0e
RokwonK <wonrok97@naver.com>
2023-03-25 16:03:50 +0900

    ✨ #140 - solution comment entity 추가

---
+++ src/modules/solution/entities/solution-comment.entity.ts
@@ -0,0 +1,19 @@
+import { ApiProperty, IntersectionType } from '@nestjs/swagger';
+import { LessonComment, LessonSolutionComment } from '@prisma/client';
+import { DateResponseType } from '@src/types/date-response.type';
+import { IdResponseType } from '@src/types/id-response-type';
+import { CommentBaseEntity } from '../../comment/entities/comment.entity';
+
+export class SolutionCommentEntity
+  extends IntersectionType(
+    IntersectionType(IdResponseType, DateResponseType),
+    CommentBaseEntity,
+  )
+  implements LessonSolutionComment
+{
+  @ApiProperty({
+    description: '댓글이 달린 문제-풀이 고유 ID',
+    example: 1,
+  })
+  lessonSolutionId: number;
+}
