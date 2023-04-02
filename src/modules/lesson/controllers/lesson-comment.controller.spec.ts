e20757b4aa03d50ec90b22fa1da88d914d50e93b
subro <clark9810@naver.com>
2023-04-02 20:14:23 +0900

    :white_check_mark: #152 - readManyComment test code
    댓글 목록조회 test code 작성 완료

--- src/modules/lesson/controllers/lesson-comment.controller.spec.ts
+++ src/modules/lesson/controllers/lesson-comment.controller.spec.ts
@@ -15,6 +15,7 @@ import {
 import { LessonCommentParamDto } from '../dtos/comment/lesson-comment-param.dto';
 import { LessonCommentEntity } from '../entities/lesson-comment.entity';
 import { LessonCommentController } from './lesson-comment.controller';
+import { ReadManyCommentQueryBaseDto } from '@src/modules/comment/dtos/read-many-comment-query-base.dto';
 
 describe('LessonCommentController', () => {
   let lessonCommentController: LessonCommentController;
@@ -186,26 +187,32 @@ describe('LessonCommentController', () => {
   describe('readManyComment', () => {
     let param: IdRequestParamDto;
     let lessonComments: LessonCommentEntity[];
+    let query: ReadManyCommentQueryBaseDto;
 
     beforeEach(() => {
       param = new IdRequestParamDto();
       lessonComments = [new LessonCommentEntity()];
+      query = new ReadManyCommentQueryBaseDto();
 
       commentService.readManyComment.mockReturnValue(lessonComments);
     });
 
     it('success - check method called', async () => {
-      await lessonCommentController.readManyComment(param);
+      await lessonCommentController.readManyComment(param, query);
 
       expect(commentService.readManyComment).toBeCalledTimes(1);
       expect(commentService.readManyComment).toBeCalledWith(
         ModelName.LessonComment,
         { lessonId: param.id },
+        query,
       );
     });
 
     it('success - check Input & Output', async () => {
-      const returnValue = await lessonCommentController.readManyComment(param);
+      const returnValue = await lessonCommentController.readManyComment(
+        param,
+        query,
+      );
 
       expect(returnValue).toStrictEqual({ lessonComments });
     });
