4244005bf2f33bd3ac7dac7912e2a5c9368a16b4
RokwonK <wonrok97@naver.com>
2023-04-08 18:52:58 +0900

    ✨ #151 - solution readMany, readOne Swagger 추가

--- src/modules/solution/controllers/solution.controller.swagger.ts
+++ src/modules/solution/controllers/solution.controller.swagger.ts
@@ -1,8 +1,41 @@
-import { applyDecorators } from "@nestjs/common";
-import { ApiCreatedResponse, ApiOkResponse, ApiOperation } from "@nestjs/swagger";
-import { SolutionEntity } from "../entities/solution.entity";
+import { HttpStatus, applyDecorators } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { SolutionEntity } from '../entities/solution.entity';
+import { ApiSuccessResponse } from '@src/decorators/api-success-response.decorator';
+import { ApiFailureResponse } from '@src/decorators/api-failure-response.decorator';
+import { HTTP_ERROR_MESSAGE } from '@src/constants/constant';
+import { ReadOneSolutionEntity } from '../entities/read-one-solution.entity';
 
-export const ApiCreateSolution = (summary: string) => applyDecorators(
-  ApiOperation({ summary }),
-  ApiCreatedResponse({ type: SolutionEntity })
-);
+export const ApiCreateSolution = (summary: string) =>
+  applyDecorators(
+    ApiOperation({ summary }),
+    ApiCreatedResponse({ type: SolutionEntity }),
+  );
+
+export const ApiReadOneSolution = (summary: string) =>
+  applyDecorators(
+    ApiOperation({ summary }),
+    ApiSuccessResponse(HttpStatus.OK, {
+      solution: { type: ReadOneSolutionEntity },
+    }),
+    ApiFailureResponse(HttpStatus.NOT_FOUND, HTTP_ERROR_MESSAGE.NOT_FOUND),
+  );
+
+export const ApiReadManySolution = (summary: string) =>
+  applyDecorators(
+    ApiOperation({ summary }),
+    ApiSuccessResponse(
+      HttpStatus.OK,
+      {
+        solutions: { type: SolutionEntity, isArray: true },
+      },
+      {
+        totalCount: { type: 'number', example: 1 },
+      },
+    ),
+    ApiFailureResponse(HttpStatus.NOT_FOUND, HTTP_ERROR_MESSAGE.NOT_FOUND),
+  );
