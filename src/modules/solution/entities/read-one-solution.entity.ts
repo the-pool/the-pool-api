f725508b86bb2e4bd6ac57278e92d470db18e20f
RokwonK <wonrok97@naver.com>
2023-04-08 18:54:18 +0900

    ✨ #151 - solution readOne entity 추가

---
+++ src/modules/solution/entities/read-one-solution.entity.ts
@@ -0,0 +1,31 @@
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { SolutionEntity } from './solution.entity';
+import { Exclude, Expose } from 'class-transformer';
+import { MemberEntity } from '@src/modules/member/entities/member.entity';
+import { SolutionCommentEntity } from './solution-comment.entity';
+import { SolutionHashtagMappingEntity } from './solution-hashtag-mapping.entity';
+import { SolutionLikeEntity } from './solution-like.entity';
+import { SolutionHashtagEntity } from './solution-hashtag.entity';
+
+export class ReadOneSolutionEntity extends SolutionEntity {
+  @Exclude()
+  memberId: number;
+
+  @ApiProperty({
+    description: '문제-풀이 정보',
+    type: MemberEntity,
+  })
+  member?: MemberEntity;
+
+  @ApiProperty({
+    example: true,
+    description: '문제-풀이 좋아요 여부',
+  })
+  @Expose()
+  get isLike(): boolean {
+    return !!this.lessonSolutionLikes?.length;
+  }
+
+  @Exclude({ toPlainOnly: true })
+  lessonSolutionLikes?: SolutionLikeEntity[] | null;
+}
