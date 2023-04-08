53fea95c1444b83196957f8de97a21bafd6bfaa9
RokwonK <wonrok97@naver.com>
2023-04-08 18:54:08 +0900

    ✨ #151 - solution readMany entity 추가

---
+++ src/modules/solution/entities/read-many-solution.entity.ts
@@ -0,0 +1,25 @@
+import { MemberEntity } from '@src/modules/member/entities/member.entity';
+import { SolutionEntity } from './solution.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { setObjectValuesToNumber } from '@src/common/common';
+import { SOLUTION_VIRTUAL_COLUMN_FOR_READ_MANY } from '../constants/solution.const';
+
+export class ReadManySolutionEntity extends SolutionEntity {
+  @ApiProperty({
+    description: '문제-풀이자',
+    type: MemberEntity,
+  })
+  member?: MemberEntity;
+
+  @ApiProperty({
+    description: '문제-풀이의 좋아요, 댓글',
+    example: setObjectValuesToNumber(
+      SOLUTION_VIRTUAL_COLUMN_FOR_READ_MANY,
+      1,
+      100,
+    ),
+  })
+  _count: {
+    [key in keyof typeof SOLUTION_VIRTUAL_COLUMN_FOR_READ_MANY]: number;
+  };
+}
