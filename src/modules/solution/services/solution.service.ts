0008e7a0aa1577b820c47f76d2dfc51e850a97a0
LeeSeokHo <rrgks@naver.com>
2023-04-15 15:23:56 +0900

    :sparkles: #154 - 솔루션 통계 find method 생성

--- src/modules/solution/services/solution.service.ts
+++ src/modules/solution/services/solution.service.ts
@@ -1,24 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@src/modules/core/database/prisma/prisma.service';
+import { LessonSolutionStatisticsResponseBodyDto } from '@src/modules/solution/dtos/lesson-solution-statistics-response-body.dto';
+import { LessonSolutionRepository } from '@src/modules/solution/repositories/lesson-solution.repository';
 import { CreateSolutionRequestBodyDto } from '../dtos/create-solution-request-body.dto';
 import { SolutionEntity } from '../entities/solution.entity';
-
 
 @Injectable()
 export class SolutionService {
   constructor(
-    private readonly prismaService: PrismaService
-  ) { }
+    private readonly lessonSolutionRepository: LessonSolutionRepository,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   createSolution(
     requestDto: CreateSolutionRequestBodyDto,
-    memberId: number
+    memberId: number,
   ): Promise<SolutionEntity> {
     return this.prismaService.lessonSolution.create({
       data: {
         ...requestDto,
-        memberId
-      }
-    })
+        memberId,
+      },
+    });
+  }
+
+  async findStatisticsByMemberId(
+    memberId: number,
+  ): Promise<LessonSolutionStatisticsResponseBodyDto> {
+    const statisticsList =
+      await this.lessonSolutionRepository.findStatisticsByMemberId(memberId);
+
+    return statisticsList[0];
   }
 }
