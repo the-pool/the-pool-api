f9977f4272dce46a2c8e8915ee90f43a54c23551
LeeSeokHo <rrgks@naver.com>
2023-04-15 15:22:35 +0900

    :sparkles: #154 - dto 생성

---
+++ src/modules/solution/dtos/lesson-solution-statistics-response-body.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+
+export class LessonSolutionStatisticsResponseBodyDto {
+  @Exclude()
+  total_day: BigInteger;
+
+  @Exclude()
+  specific_month_day: BigInteger;
+
+  @Exclude()
+  total_count: BigInteger;
+
+  @Exclude()
+  specific_month_count: BigInteger;
+
+  constructor(partial?: Partial<LessonSolutionStatisticsResponseBodyDto>) {
+    Object.assign(this, partial || {});
+  }
+
+  @ApiProperty({
+    description: '멤버의 풀이를 한 전체 일',
+  })
+  @Expose()
+  get totalDay(): number {
+    return Number(this.total_day);
+  }
+
+  @Expose()
+  @ApiProperty({
+    description: '멤버의 이번달 풀이를 한 일',
+  })
+  get specificMonthDay(): number {
+    return Number(this.specific_month_day);
+  }
+  @ApiProperty({
+    description: '멤버의 전체 풀이 개수',
+  })
+  @Expose()
+  get totalCount(): number {
+    return Number(this.total_count);
+  }
+  @ApiProperty({
+    description: '멤버의 이번달 풀이 개수',
+  })
+  @Expose()
+  get specificMonthCount(): number {
+    return Number(this.specific_month_count);
+  }
+}
