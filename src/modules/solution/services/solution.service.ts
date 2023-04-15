20c19ed1120eb5b9c0480878cc6b419064086049
RokwonK <wonrok97@naver.com>
2023-04-15 17:47:32 +0900

    🎨 디버그 코드 제거

--- src/modules/solution/services/solution.service.ts
+++ src/modules/solution/services/solution.service.ts
@@ -66,8 +66,6 @@ export class SolutionService {
       [sortBy]: settledOrderBy,
     });
 
-    console.log('과연');
-
     // promise 한 lesson 목록
     const readManyLessonQuery: PrismaPromise<ReadManySolutionEntity[]> =
       this.prismaService.lessonSolution.findMany({
