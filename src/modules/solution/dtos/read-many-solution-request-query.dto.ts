618b2330c0ef450f3927fed2a47d35c517ca9466
RokwonK <wonrok97@naver.com>
2023-04-15 17:10:47 +0900

    🎨 solution 목록 받아오기 dto transform 로직 수정

--- src/modules/solution/dtos/read-many-solution-request-query.dto.ts
+++ src/modules/solution/dtos/read-many-solution-request-query.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty, IntersectionType } from '@nestjs/swagger';
 import { PageDto } from '@src/dtos/page.dto';
 import { SortDto } from '@src/dtos/sort.dto';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsInt, IsOptional } from 'class-validator';
 import { SOLUTION_VIRTUAL_COLUMN_FOR_READ_MANY } from '../constants/solution.const';
 import { EntityId } from '@src/constants/enum';
+import { Type } from 'class-transformer';
 
 export class ReadManySolutionRequestQueryDto extends IntersectionType(
   PageDto,
@@ -11,13 +12,21 @@ export class ReadManySolutionRequestQueryDto extends IntersectionType(
 ) {
   @ApiProperty({
     description: '연관된 문제 고유 Id',
+    required: false,
+    type: 'number',
   })
+  @IsInt()
+  @Type(() => Number)
   @IsOptional()
   lessonId?: number;
 
   @ApiProperty({
     description: '유저 고유 Id',
+    required: false,
+    type: 'number',
   })
+  @IsInt()
+  @Type(() => Number)
   @IsOptional()
   memberId?: number;
 
