a3ffc685b5b305d1dc2c8fce0e1e82d315f2fafe
subro <clark9810@naver.com>
2023-04-22 15:04:37 +0900

    :fire: #165 - remove not use annotation
    사용하지 않는 주석 삭제

--- src/modules/comment/dtos/create-comment-base.dto.ts
+++ src/modules/comment/dtos/create-comment-base.dto.ts
@@ -5,7 +5,6 @@ import { CommentBaseEntity } from '../entities/comment.entity';
 export class CreateCommentBaseDto extends PickType(CommentBaseEntity, [
   'description',
 ]) {
-  // 댓글 길이에 관한 정책이 정해지면 Length 데코레이터가 추가되야 함
   @MaxLength(500)
   @IsString()
   @IsNotEmpty()
