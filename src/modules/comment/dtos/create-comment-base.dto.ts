3e6f68bea6e04dc5bbde679735a2063a1c5db8ec
subro <clark9810@naver.com>
2023-04-22 15:03:49 +0900

    :recycle: #165 - comment description to accept up to 500 characters
    댓글 설명 500자까지 제한될 수 있도록 수정

--- src/modules/comment/dtos/create-comment-base.dto.ts
+++ src/modules/comment/dtos/create-comment-base.dto.ts
@@ -1,11 +1,12 @@
 import { PickType } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 import { CommentBaseEntity } from '../entities/comment.entity';
 
 export class CreateCommentBaseDto extends PickType(CommentBaseEntity, [
   'description',
 ]) {
   // 댓글 길이에 관한 정책이 정해지면 Length 데코레이터가 추가되야 함
+  @MaxLength(500)
   @IsString()
   @IsNotEmpty()
   description: string;
