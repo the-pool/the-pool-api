62cc0c89a6d5e30397dc3adfc75026acebf25b10
LeeSeokHo <rrgks@naver.com>
2023-05-06 13:14:26 +0900

    :bug: #175 - env notification value making 하여 처리

--- src/modules/core/notification/notification.module.ts
+++ src/modules/core/notification/notification.module.ts
@@ -5,6 +5,7 @@ import {
   WEBHOOK_CLIENT_TOKEN,
 } from '@src/modules/core/notification/constants/notification.constant';
 import { NotificationService } from '@src/modules/core/notification/services/notification.service';
+import { Field } from '@src/modules/core/notification/type/notification.type';
 import { ENV_KEY } from '@src/modules/core/the-pool-config/constants/the-pool-config.constant';
 import { ThePoolConfigService } from '@src/modules/core/the-pool-config/services/the-pool-config.service';
 import { EmbedBuilder, WebhookClient } from 'discord.js';
@@ -36,8 +37,27 @@ export class NotificationModule implements OnApplicationBootstrap {
       return;
     }
 
+    const envs = Object.entries(this.thePoolConfigService.getAllMap());
+
     const notificationService =
       this.moduleRef.get<NotificationService>(NotificationService);
+
+    const maskedEnvFields: Field[] = envs.map(([key, value]) => {
+      const maskedValue: string = [...String(value)]
+        .map((str, idx) => {
+          if (idx % 3 === 2) {
+            return String(str);
+          }
+          return '\\*';
+        })
+        .join('');
+
+      return {
+        name: key,
+        value: maskedValue,
+        inline: false,
+      };
+    });
 
     await notificationService
       .send(
@@ -48,15 +68,7 @@ export class NotificationModule implements OnApplicationBootstrap {
           color: '#33FF68', // 연두
           title: 'Build Success',
           description: 'Current Environment Variable List',
-          fields: Object.entries(this.thePoolConfigService.getAllMap()).map(
-            ([key, value]) => {
-              return {
-                name: key,
-                value: String(value),
-                inline: false,
-              };
-            },
-          ),
+          fields: maskedEnvFields,
         },
       )
       .catch((e) => {
